405c9d267d0acf943da3ab284606974412621fbc
luiz-Guilherme1604 <110949935+luiz-Guilherme1604@users.noreply.github.com>
2023-02-10 14:28:30 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/* Select all required elements */
+const iconMenu = document.querySelector(".btn i")
+const menu = document.querySelector("div.items")
+
+iconMenu.addEventListener('click', function() {
+    iconMenu.classList.toggle('show')
+    menu.classList.toggle('show')
+});
+
